fd66597c0242368794c2762e5f8abf5c6b06eb4f
saadjumani <saadjumani@hotmail.com>
2017-10-27 16:49:06 +0500

    Added script template

--- Assets/GameplayScripts/CameraControl.cs
+++ Assets/GameplayScripts/CameraControl.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 //Syed Raahim Bukhari
 //Adnan Anwar Ali
-//Abdul Rehman
 // code written by Muhammad Saad Jumani
 // this is an edited comment
 
---
+++ Assets/MoveToTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour {
+
+    public GameObject target;
+    Vector3 position;       //[x,yz]
+    // Use this for initialization
+    void Start () {
+        position = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
